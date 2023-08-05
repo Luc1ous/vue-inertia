01cdd6d5d5d80861e756dcafeecef12dec45e909
Luc1ous <ardianilyas@gmail.com>
2023-08-05 18:05:27 +0700

    change font

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
+    <link href="https://fonts.googleapis.com/css2?family=Albert+Sans:ital,wght@0,100;0,500;0,700;1,600&display=swap" rel="stylesheet"> 
+
     <!-- Fetch project name dynamically -->
     <title>Vue Inertia</title>
 
@@ -11,7 +13,13 @@
     @vite('resources/js/app.js') @inertiaHead
   </head>
 
-  <body class="font-inter antialiased bg-gray-50 dark:bg-gray-900 dark:text-white">
+  <style>
+    body{
+      font-family: 'Albert Sans', sans-serif;
+    }
+  </style>
+
+  <body class="antialiased bg-gray-50 dark:bg-gray-900 dark:text-white">
     @inertia
   </body>
 </html>
